ea82ab9585376a233c54df565447ff570fcb8021
wucheng <wc@candypay.com>
2022-07-12 15:34:53 +0800

    添加nacos 配置中心功能

--- provider-server-8001/src/main/java/com/wcn/alibaba/provider/HttpDemoController.java
+++ provider-server-8001/src/main/java/com/wcn/alibaba/provider/HttpDemoController.java
@@ -1,19 +1,30 @@
 package com.wcn.alibaba.provider;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HttpDemoController {
 
     @Value("${server.port}")
     private int port;
+
+    @Value("${dbName}")//nacos中配置
+    private String dbName;
 
     @RequestMapping("/demo")
     public String demo(){
         String ret = "This is Http Demo. port="+port;
         System.out.println(ret);
         return ret;
+    }
+
+    @RequestMapping("/getConfig")
+    public String getConfig(){
+        System.out.println(this);
+        return "my port="+port+"。 dbName="+dbName;
     }
 }
